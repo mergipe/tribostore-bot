2f59d893fad4455cdcbb7a611ebaaf4b56582a58
Gustavo Mendes Maciel <gmmaciel.bh2@gmail.com>
2020-06-20 20:09:55 -0300

    Removed try/except in test_config_url

--- test/test_config.py
+++ test/test_config.py
@@ -5,9 +5,5 @@ from tribostorebot.config import Config
 
 def test_config_url():
     cfg = Config()
-
-    try:
-        resp = requests.get(cfg.request_url)
-        assert resp.ok
-    except:
-        pytest.fail("Invalid request url.")
+    resp = requests.get(cfg.request_url)
+    assert resp.ok
