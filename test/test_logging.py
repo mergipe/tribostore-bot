64aafade7a463746e9799be55c5d7b4cbd298a0e
Gustavo Mendes Maciel <gmmaciel.bh2@gmail.com>
2020-06-20 00:29:39 -0300

    Testing logging config

---
+++ test/test_logging.py
@@ -0,0 +1,12 @@
+import run
+import sys
+
+def test_logging_config(tmp_path):
+    run.LOG_PATH = tmp_path
+    run._config_logging()
+    assert len(list(tmp_path.iterdir())) == 1
+
+    log_filepath = list(tmp_path.iterdir())[0]
+    with open(log_filepath, 'w') as logfile:
+        assert sys.stdout.name == logfile.name
+        assert sys.stderr.name == logfile.name
