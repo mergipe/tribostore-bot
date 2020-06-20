54109da32ce2e08d39f3e813b2ce4dc69d7f3a87
Gustavo Mendes Maciel <gmmaciel.bh2@gmail.com>
2020-06-20 00:28:59 -0300

    Removed assert parenthesis

--- test/test_paths.py
+++ test/test_paths.py
@@ -1,13 +1,13 @@
 import definitions
 
 def test_data_path():
-    assert(definitions.DATA_PATH.exists())
-    assert(definitions.DATA_PATH.is_dir())
+    assert definitions.DATA_PATH.exists()
+    assert definitions.DATA_PATH.is_dir()
 
 def test_log_path():
-    assert(definitions.LOG_PATH.exists())
-    assert(definitions.LOG_PATH.is_dir())
+    assert definitions.LOG_PATH.exists()
+    assert definitions.LOG_PATH.is_dir()
 
 def test_config_filepath():
-    assert(definitions.CONFIG_FILEPATH.exists())
-    assert(definitions.CONFIG_FILEPATH.is_file())
+    assert definitions.CONFIG_FILEPATH.exists()
+    assert definitions.CONFIG_FILEPATH.is_file()
