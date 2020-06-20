6f1c04cc41bb6f1d95088d650c08834b17e323c2
Gustavo Mendes Maciel <gmmaciel.bh2@gmail.com>
2020-06-19 22:45:05 -0300

    Creates data/ and log/ folders if don't exist

--- definitions.py
+++ definitions.py
@@ -3,3 +3,9 @@ from pathlib import Path
 CONFIG_FILEPATH = Path('.') / 'config' / 'config.ini'
 LOG_PATH = Path('.') / 'log'
 DATA_PATH = Path('.') / 'data'
+
+if not LOG_PATH.exists():
+    Path.mkdir(LOG_PATH)
+
+if not DATA_PATH.exists():
+    Path.mkdir(DATA_PATH)
